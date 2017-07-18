56c51015a729787e69de11d59bdf19fbe1a55005
Emre Eroglu <emre@emreeroglu.net>
2017-07-18 23:56:53 +0300

    Change connection limit.

--- build/src/server.cpp
+++ build/src/server.cpp
@@ -415,8 +415,10 @@ static struct MHD_Daemon *init_daemon() {
 	
 	// broken? (see below)
 	// unsigned int conn_timeout = 30;
-	unsigned int conn_limit = 40960;
-	unsigned int listen_queue_size = 10240;
+    // default value was 4096
+    unsigned int conn_limit = UINTMAX_MAX;
+    // default value was 1024
+    unsigned int listen_queue_size = 10240;
 
 	if (USE_SSL)
 		return MHD_start_daemon( flags, port, apc, apc_cls, dh, dh_cls,
