4f2ff68b978b11812a348fcc8ba900b84578eca1
Emre Eroglu <emre@emreeroglu.net>
2017-08-17 01:31:41 +0300

    Prevent deadlock.

--- build/include/WebSocketHandler.hpp
+++ build/include/WebSocketHandler.hpp
@@ -228,7 +228,8 @@ void WebSocketHandler<SUBCLASS>::async_wake_up() {
 		std::cerr  << ftime() << "[WebSocketHandler::async_wake_up] Too many connections! " << e.getErrorCode() << ": " << e.what() << std::endl;
 	} catch(const std::exception &e) {
 		// we still need to release mutex
-		std::cerr  << ftime() << "[WebSocketHandler::async_wake_up] " << e.what() << std::endl;
+        connections_mutex.unlock();
+		std::cerr  << ftime() << "[WebSocketHandler::async_wake_up] Exception :" << e.what() << std::endl;
 		throw(e);
 	}
 }
